82f9d2b9e6c5ac640c188291df16222070027ab8
Jim <caijim@github.com>
2015-05-07 20:28:43 -0700

    first commit

---
+++ tinder/DraggableView.swift
@@ -0,0 +1,21 @@
+//
+//  DraggableView.swift
+//  tinder
+//
+//  Created by Jim Cai on 5/7/15.
+//  Copyright (c) 2015 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class DraggableView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
