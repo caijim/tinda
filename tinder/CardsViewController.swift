fb7dfdcf911d361b5479bf81760a49ed8fbda28b
Jim <caijim@github.com>
2015-05-07 20:28:50 -0700

    bluh

--- tinder/CardsViewController.swift
+++ tinder/CardsViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 
 class CardsViewController: UIViewController {
-
+    var cardInitialCenter: CGPoint!
+    
+    @IBOutlet weak var card: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
--- tinder/DraggableImageView.swift
+++ tinder/DraggableImageView.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class DraggableImageView: UIView {
 
+    @IBOutlet var chill: UIView!
+    
+    var cardInitialCenter: CGPoint!
+    
+    @IBOutlet weak var card: UIImageView!
+    
+    
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
@@ -17,5 +24,49 @@ class DraggableImageView: UIView {
         // Drawing code
     }
     */
+    
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSubviews()
+    }
+    
+    func initSubviews() {
+        // standard initialization logic
+        let nib = UINib(nibName: "DraggableImageView", bundle: nil)
+        nib.instantiateWithOwner(self, options: nil)
+        chill.frame = bounds
+        addSubview(chill)
+        
+        // custom initialization logic
+    }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func onImageDragged(sender: UIPanGestureRecognizer) {
+        var point = sender.locationInView(sender.view)
+        var velocity = sender.velocityInView(sender.view)
+        var translation = sender.translationInView(sender.view!)
+        if sender.state == UIGestureRecognizerState.Began {
+            cardInitialCenter = card.center
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            card.center = CGPoint(x: self.cardInitialCenter.x+translation.x, y:  cardInitialCenter.y + translation.y)
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 1.0, initialSpringVelocity: 0, options: nil, animations: { () -> Void in
+                self.card.center =  CGPoint(x: self.cardInitialCenter.x, y: self.cardInitialCenter.y)
+                }, completion: { (finished: Bool) -> Void in
+            })
+            
+            
+            
+        }
+    }
 
 }
